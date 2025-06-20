349e698b9de3404ed47936d1316f237a8ca76714
Igor Poubel <ipoubellopes@gmail.com>
2025-06-19 23:58:24 -0300

    ajustes no zshrc e no init_install.sh

--- .zshrc
+++ .zshrc
@@ -1,6 +1,10 @@
 # If you come from bash you might have to change your $PATH.
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
+if [ -d "$HOME/.yarn/bin" ]; then
+  export PATH="$PATH:$HOME/.yarn/bin"
+fi
+
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
@@ -70,7 +74,11 @@ ZSH_THEME="af-magic"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(
+  git
+  zsh-autosuggestions
+	zsh-syntax-highlighting
+)
 
 source $ZSH/oh-my-zsh.sh
 
--- init_installs.sh
+++ init_installs.sh
@@ -4,17 +4,24 @@ echo "Installing packages..."
 echo "Install FNM..."
 
 # Install fnm
-# curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
-# echo -e '\n# fnm
-# FNM_PATH="$HOME/.local/share/fnm"
-# if [ -d "$FNM_PATH" ]; then
-#   export PATH="$HOME/.local/share/fnm:$PATH"
-#   eval "`fnm env`"
-# fi' >> $HOME/.zshrc
+echo -e '\n# fnm
+FNM_PATH="$HOME/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="$HOME/.local/share/fnm:$PATH"
+  eval "`fnm env`"
+fi' >> $HOME/.zshrc
 
-# echo -e '\neval "$(fnm env --use-on-cd --shell zsh)"' >> $HOME/.zshrc
+echo -e '\neval "$(fnm env --use-on-cd --shell zsh)"' >> $HOME/.zshrc
 
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Installing oh-my-zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
 
